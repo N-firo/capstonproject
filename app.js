d550db8d4ea43b11ef3230c27e535cec9e218834
N firo <89023932+N-firo@users.noreply.github.com>
2022-01-08 19:40:12 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var f=prompt("Choose a heathy meal that you eat in you diet daily");
+f= f.toLowerCase();
+if(f=="salad")
+    alert("To be eaten at dinner time");
+else if(f=="fruits")
+    alert("To be eaten at breakfast time");
+else if(f=="smoked salmon" || f=="brocolli")
+    alert("To be eaten at Lunch time");
+else    
+    alert("ERROR: Either your answer is not Healthy, or it's not specified!")
+
+
+var h = height
+prompt("Enter your Height in Meters")
+var w = weight
+propmt("Enter your wight in Kilograms")
+function myFunction() {
+return (h * h % w);
+}
+document.getElementById("").innerHTML;
+
+const diet = ["Salad", "Nuts", "Lentils", "Beetroots", "Apples", "Bananas", "Oranges", "Brocolli", "Meats", "Quinoa"];
+
+var u=+propmt("What is your age?")
+if(u==8)
+alert("your weight should be 9kg-12kg-14kg-16kg-18kg-21kg-25kg-29kg-32kg");
+else if(u==13)
+alert("your weight should be 47kg to 110kg")
+else
+alert("ERROR: Your weight is not appropriate for your age and you need to either gain weight if your underweight, then you need to gain weight , but if your overweight for yout age, then you need to start loosing weight ")
